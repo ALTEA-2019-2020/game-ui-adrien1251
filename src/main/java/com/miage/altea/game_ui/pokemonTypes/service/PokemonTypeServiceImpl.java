2d4ae900a4e19a30702bfa46b515bae6bc5f6425
Adrien Deblock <adrien.deblock@davidson.fr>
2020-02-10 10:47:53 +0100

    feat(translations): adding translation

--- src/main/java/com/miage/altea/game_ui/pokemonTypes/service/PokemonTypeServiceImpl.java
+++ src/main/java/com/miage/altea/game_ui/pokemonTypes/service/PokemonTypeServiceImpl.java
@@ -3,7 +3,12 @@ package com.miage.altea.game_ui.pokemonTypes.service;
 import com.miage.altea.game_ui.pokemonTypes.bo.PokemonType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
@@ -16,20 +21,29 @@ public class PokemonTypeServiceImpl implements PokemonTypeService {
 
     private RestTemplate restTemplate;
 
-
     private String pokemonServiceUrl;
 
     @Override
     public List<PokemonType> listPokemonsTypes() {
-        PokemonType[] pokemonTypes = restTemplate.getForObject(pokemonServiceUrl + POKEMON_TYPE_PATH, PokemonType[].class);
+        MultiValueMap<String, String> headers = new HttpHeaders();
+        headers.add("Accept-Language", LocaleContextHolder.getLocale().toLanguageTag());
+        HttpEntity entity = new HttpEntity(headers);
+        PokemonType[] pokemonTypes = restTemplate
+                .exchange(pokemonServiceUrl + POKEMON_TYPE_PATH, HttpMethod.GET, entity, PokemonType[].class)
+                .getBody();
+
         return Arrays.asList(pokemonTypes);
     }
 
     @Override
     public List<PokemonType> listPokemonsTypes(List<Integer> ids) {
+        MultiValueMap<String, String> headers = new HttpHeaders();
+        headers.add("Accept-Language", LocaleContextHolder.getLocale().toLanguageTag());
+        HttpEntity entity = new HttpEntity(headers);
+
         List<PokemonType> pokemonTypes = new ArrayList<>();
         ids.stream().forEach(pokemonId ->
-                pokemonTypes.add(restTemplate.getForObject(pokemonServiceUrl + POKEMON_TYPE_PATH + pokemonId, PokemonType.class))
+                pokemonTypes.add(restTemplate.exchange(pokemonServiceUrl + POKEMON_TYPE_PATH + pokemonId, HttpMethod.GET, entity, PokemonType.class).getBody())
         );
         return pokemonTypes;
     }
