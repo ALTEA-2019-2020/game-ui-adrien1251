70d3b932dc74ab03592332724fed67c32345179a
deblocka <fdbxf8ixfn>
2020-03-23 09:05:46 +0100

    feat(fight)

---
+++ src/main/java/com/miage/altea/game_ui/controller/BattleController.java
@@ -0,0 +1,25 @@
+package com.miage.altea.game_ui.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.security.Principal;
+
+@Controller
+@RequestMapping("/fight")
+@CrossOrigin(origins = "*")
+public class BattleController {
+    @GetMapping("/{opponent}")
+    public ModelAndView fight(Principal principal, @PathVariable String opponent) {
+        var modelAndView = new ModelAndView("fight");
+
+        modelAndView.addObject("trainerName", principal.getName());
+        modelAndView.addObject("opponentName", opponent);
+
+        return modelAndView;
+    }
+}
